fa39c8a6ce0a35b31307e81113efb1c63af45bb1
Paul Ouellette PC <pouellette25@gmail.com>
2017-02-26 07:05:58 -0500

    Decoupling Your Architecture

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -34,7 +34,7 @@ public:
 
 protected:
 	UPROPERTY(BlueprintReadOnly, Category = "State")
-		EFiringState FiringState = EFiringState::Reloading;
+		EFiringState FiringState = EFiringState::Aiming;
 
 private:
 	UTankBarrel* Barrel = nullptr;
